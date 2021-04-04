7e632a22096473bfb53f1dc07ae4e261ebae5e11
shoichifunyu <51820226+Shoichifunyu@users.noreply.github.com>
2021-04-04 11:51:55 +0900

    機能追加

--- app/src/main/java/com/example/myscheduler/FirstFragment.kt
+++ app/src/main/java/com/example/myscheduler/FirstFragment.kt
@@ -7,18 +7,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myscheduler.databinding.FragmentFirstBinding
+import io.realm.Realm
+import io.realm.kotlin.where
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var realm: Realm
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        realm = Realm.getDefaultInstance()
+    }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,5 +39,20 @@ class FirstFragment : Fragment() {
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        binding.list.layoutManager = LinearLayoutManager(context)
+        val schedules = realm.where<Schedule>().findAll()
+        val adapter = ScheduleAdapter(schedules)
+        binding.list.adapter = adapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
--- app/src/main/java/com/example/myscheduler/MyScedulerApplication.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myscheduler
-
-class MyScedulerApplication {
-}
